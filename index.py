8eb4f0ba1f6ee429520346ad4ce0075838376cf3
Yuya Soneoka <ej.a9p96@gmail.com>
2014-04-12 13:20:54 +0900

    add fb auth

--- index.py
+++ index.py
@@ -5,13 +5,21 @@ import sys
 import os
 sys.path.append('libs')
 
-from bottle import route, static_file, run, template, static_file, view
+from bottle import route, static_file, run, template, static_file, view,Bottle
+import bottle_mysql
 # from bottle import route, static_file, default_app
 # from app.controllers import *
 
 # ====================================
 #  adding static path
 # ====================================
+
+app = Bottle()
+# dbhost is optional, default is localhost
+plugin = bottle_mysql.Plugin(dbuser='ai', dbpass='miyuki', dbname='groupmaker')
+app.install(plugin)
+
+
 @route('/static/<filepath:path>')
 def server_static(filepath):
 	return static_file(filepath, root='./static/')
