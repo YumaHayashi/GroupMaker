a929237f428e2c9571fb8c0203dd7c84fb851530
Yuma <yuma@yuma-note-Ubuntu.(none)>
2014-04-13 13:05:58 +0900

    Modify graph drawing module.

--- app/model/draw_graph.py
+++ app/model/draw_graph.py
@@ -8,14 +8,19 @@ import sys
 sys.path.append('../../libs')
 import get_db
 
+dbcon, dbcur = get_db.get_db()
+dbcur.execute('select id,name from users')
+user_list = dbcur.fetchall()
+
+id_name_map = {k:v for k, v in user_list}
 
 connection = make_group.get_user_connection()
 
 G = networkx.Graph()
-G.add_nodes_from(range(1,9))
+G.add_nodes_from(id_name_map.values())
 # G.add_edges_from(map(lambda x: x[0], connection))
 for pair, w in connection:
-    G.add_edge(pair[0], pair[1], weight=w)
+    G.add_edge(id_name_map[pair[0]], id_name_map[pair[1]], weight=w)
 
 networkx.draw(G)
 plt.show()
